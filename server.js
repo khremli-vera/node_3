c291285f1b0ee95d9e1b1d974faab8531fb999cf
Vera Khremli <lovet.post@gmail.com>
2024-12-07 13:22:06 +0300

    feat: add validation to uri

--- server.js
+++ server.js
@@ -6,6 +6,9 @@ app.use(express.json());
 
 app.use('/', filmsRouter);
 app.use('/:id', filmsRouter);
+app.use('/*', function (req, res) {
+    res.status(400).send("Invalid request")
+})
 
 
 app.listen(3000, '127.0.0.1', function () {
